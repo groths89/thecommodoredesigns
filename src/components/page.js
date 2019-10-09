d2161d80d13c37048607eb5207cb6aa66364e0d9
groths89 <g.p.rothstein@gmail.com>
2019-10-09 16:51:10 -0400

    commit

--- src/components/page.js
+++ src/components/page.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import Link from 'gatsby-link'
 
@@ -13,4 +14,21 @@ const Page = ({ siteTitle }) =>(
     </div>
 )
 
+=======
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Page = ({ siteTitle }) =>(
+    <div class="page"
+    style={{
+        width: 'auto',
+	    height: 'auto',
+	    margin: '0 auto 0 0',
+    }}
+    >
+
+    </div>
+)
+
+>>>>>>> 01953f9c9d20aa18dc0455608c062eb088d17375
 export default Page
